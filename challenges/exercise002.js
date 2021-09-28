76f79f06eb98c19cb55e5c848a66fe707d8a114e
metamiaip <metamia@gmail.com>
2021-09-29 00:21:20 +0100

    Implemented exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,32 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return JSON.parse(JSON.stringify(sandwich)).fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  return JSON.parse(JSON.stringify(person)).city =="Manchester"? true:false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  return arr.reduce((a, v) => (v === "sheep" ? a + 1 : a), 0);
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  const obj = JSON.parse(JSON.stringify(person));
+  return obj.address.postCode.charAt(0) =="M" && obj.address.city == "Manchester"? true:false;
 }
 
 module.exports = {
