1dbc5c8e3b2ea0ac9e949ec9ba3011b4a1a69d46
metamiaip <metamia@gmail.com>
2021-09-29 00:31:28 +0100

    Implemented exerrcise004

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,43 +1,51 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  return nums.filter(value => value < 1);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
   // Your code here
+  return names.filter(value => value.substring(0,1) ==char);
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   // Your code here
+  return words.filter(value => value.substring(0,3) =="to ");
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  return nums.filter( (num) => num > 0 && num % parseInt(num) === 0 );
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
   // Your code here
+  return users.map( user => user.data.city.displayName);
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  return nums.map( num => +Math.sqrt(num).toFixed(2));
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
   // Your code here
+  return sentences.filter( value => value.toLowerCase().includes(str));
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
   // Your code here
+  return triangles.map( value => Math.max(...value));
 }
 
 module.exports = {
