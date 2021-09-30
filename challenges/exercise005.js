2425bb0913bdf455b5c12f146c49b647f1e54acc
metamiaip <metamia@gmail.com>
2021-09-30 19:19:36 +0100

    Implemented exercise005 v2

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -12,7 +12,7 @@ const count1sand0s = str => {
   //Case 1:count1sand0s("11000")
   return [...str].reduce((a, e) => { a[e] = a[e] ? a[e] + 1 : 1; return a }, {});
   //or
-  return [...str].map(Number).reduce((a, e) => { a[e] = a[e] ? a[e] + 1 : 1; return a}, {});
+  //return [...str].map(Number).reduce((a, e) => { a[e] = a[e] ? a[e] + 1 : 1; return a}, {});
   //result: {"0":3,"1":2}  
   //that means convert to number array is no use in this stage
 
@@ -35,6 +35,7 @@ const count1sand0s = str => {
   In: count1sand0s("11000")
   Out: ["1","0"]
   */
+
   //not yet solved ...
 };
 
@@ -54,18 +55,44 @@ const arrShift = arr => {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
   var [first, last] = [arr.splice(0,1), arr.splice(-1)];
-  return  [...last, ...arr, ...first];
+  return  [...last, ...arr, ...first];  //this syntax is from web and really amazing to form a new array
 };
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
   // Your code here!
+  return JSON.stringify(haystack).replace(/"/g, '').toLowerCase().indexOf(searchTerm) != -1? true : false;  
 };
 
 const getWordFrequencies = str => {
   if (str === undefined) throw new Error("str is required");
   // Your code here!
+  return str.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '').match(/\w+/ig).map(r => r.toLowerCase()).reduce(function(str,r){return str.hasOwnProperty(r)?++str[r]:str[r]=1,str},{});
+
+  //json key does not contain double quotes surround it, test not pass
+  /*
+  research:
+  https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_regexp_onemore2
+  var str = "Hellooo World! Hello W3Schools!"; 
+  var patt1 = /\w+/g;
+  var result = str.match(patt1);
+
+  so str.match(/\w+/g) can break down each word of a sentence into array
+  "hello world" => ["hello","world"]
+  str.match(/\w+/g);
+
+  https://stackoverflow.com/questions/30906807/word-frequency-in-javascript
+  excellent one line example:
+  function wordCounts(n) {
+  return n.match(/\w+/g).reduce(function(n,r){return n.hasOwnProperty(r)?++n[r]:n[r]=1,n},{});
+  };
+  but cannot understand this part:
+  function(n,r){return n.hasOwnProperty(r)?++n[r]:n[r]=1,n}
+  My understanding:
+  - hasOwnProperty(curr) return true when found, false when not found
+  - ++n[r] = n[r] = n[r] + 1, increment the value of first word by 1
+  */
 };
 
 module.exports = {
