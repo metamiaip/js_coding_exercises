f3943a3f72341d192abb16cfdd017c37edb52385
metamiaip <metamia@gmail.com>
2021-10-07 21:55:58 +0100

    Exercise007 - findWinner (remove redundunt checking)

--- challenges/exercise007.js
+++ challenges/exercise007.js
@@ -131,18 +131,18 @@ const hexToRGB = hexStr => {
  */
  const findWinner = board => {
   if (board === undefined) throw new Error("board is required");
-  if (board[0][0] != null && board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == board[0][0]) { 
+  if (board[0][0] != null && board[0][0] == board[1][1] && board[1][1] == board[2][2]) { 
     return board[0][0];
   }
-  else if (board[2][0] != null && board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[0][2] == board[2][0])  { 
+  else if (board[2][0] != null && board[2][0] == board[1][1] && board[1][1] == board[0][2])  { 
     return board[2][0];
   }
   else {
     for (let i = 0; i < 3; i++) {
-      if (board[i][0] != null && board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] == board[i][0]) {
+      if (board[i][0] != null && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
         return board[i][0];
       }
-      else if (board[0][i] != null && board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[2][i] == board[0][i]) {
+      else if (board[0][i] != null && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
         return board[0][i];
       }
     }
